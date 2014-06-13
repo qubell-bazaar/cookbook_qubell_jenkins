4a45f8bc96ffd040e7a81782ec053def9d70c702
Anton Butovich <abutovich@gmail.com>
2014-06-14 01:33:16 +0300

    changed action attribute name

--- recipes/plugins_management.rb
+++ recipes/plugins_management.rb
@@ -2,6 +2,18 @@
 # Jenkins plugin management recipe
 #
 
-jenkins_plugin node['jenkins']['server']['plugins'] do
-  action :"#{node['jenkins']['server']['action']}"
+node['jenkins']['server']['plugins'].each do |plugin|
+  if plugin.is_a?(Hash)
+    name = plugin['name']
+    version = plugin['version'] if plugin['version']
+    url = plugin['url'] if plugin['url']
+  else
+    name = plugin
+  end
+
+  jenkins_plugin name do
+    action :"#{node['jenkins']['server']['plugins_action']}"
+    version version if version
+    url url if url
+  end
 end
