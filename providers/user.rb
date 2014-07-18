4f5d7eb87c9fcb2bc54848a69fc5782352300889
Anton Butovich <abutovich@gmail.com>
2014-07-18 12:52:46 +0300

    Set password and key after full restore

--- providers/user.rb
+++ providers/user.rb
@@ -15,26 +15,26 @@ end
 action :create do
   directory ::File.join(node['jenkins']['server']['home'], "users") do
     owner node['jenkins']['server']['user']
-    group 'nogroup'
+    group node['jenkins']['server']['group'] 
     action :create
   end
 
   username = new_resource.username || new_resource.id
   directory ::File.join(node['jenkins']['server']['home'], "users", username) do
     owner node['jenkins']['server']['user']
-    group 'nogroup'
+    group node['jenkins']['server']['group']
     action :create
   end
   template ::File.join(node['jenkins']['server']['home'], "users", username, "config.xml") do
     source "user.xml.erb"
     owner node['jenkins']['server']['user']
-    group 'nogroup'
+    group node['jenkins']['server']['group']
     variables(
       :fullname => new_resource.comment,
       :password => new_resource.password,
       :email => new_resource.email
     )
-    action :create_if_missing
+    action :create
   end
   new_resource.updated_by_last_action(true)
 end
